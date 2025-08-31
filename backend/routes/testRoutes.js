d5f0a661ef785b8f5eeb71ddfed998858d5bef0b
12ParthPatil <parthpatil3569@gmail.com>
2025-08-31 22:55:26 +0530

    Edited testRoute

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Test = require('../models/Test');
+const Test = require('../models/test');
 const auth = require("../middleware/auth");
 
 // POST - Add a test entry
